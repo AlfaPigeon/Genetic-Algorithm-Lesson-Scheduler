23b9e363308e0aed9dce767144ef33bd454e9e55
unknown <gatasoy@etu.edu.tr>
2020-07-21 23:05:05 +0300

    Sunum

--- Bil441_Genetic/test.py
+++ Bil441_Genetic/test.py
@@ -1,13 +1,9 @@
 import random
-from pg import DB
 import copy
 import matplotlib.pyplot as plt
 from joblib import dump, load
-from multiprocessing import Pool, freeze_support
 import time
 from libgenetic import *
-
-
 
 def gen_timehit_evaluate(gen1, gen2):
    
@@ -137,46 +133,84 @@ def evaluate_paralel(gen, genes, beta):
         gen.add_fitness(gen_break_evaluate(gen, gen2)/(gen.branch.lesson.priority*beta))
     return gen.fitness
 
-def evaluate_p(genom,cores,beta):
+def evaluate_p(genom):
 
     # Derslerin kendi aralarında çakışmaları puanlanır
     # Aynı hocanın aynı zamanda farklı yerlerde olmamalarına dikkat edilir
     # Aynı sınıfın aynı zamanda iki farklı ders için atanmadığına dikkat edilir
     # Hocaların boş zamanları ile ders zamanları uyumları kontrol edilir
 
-    total_fitness = 0
-    args=[]
     for gen in genom.genes:
-        args.append((gen, genom.genes, beta))
-    
-    p = Pool(cores)
-    fit = p.starmap(evaluate_paralel, args)
+        gen.set_fitness(0)
+        for gen2 in genom.genes:
+            gen.add_fitness(gen_timehit_evaluate(gen, gen2))
+            gen.add_fitness(gen_teacherhit_evaluate(gen, gen2))
+            gen.add_fitness(gen_roomhit_evaluate(gen, gen2))
+            gen.add_fitness(gen_teacherfreetime_evaluate(gen, gen2))
 
-
-    p.close()
-    p.join()
-
-    time.sleep(0.1)
-
-    
-    for f in fit:
-        total_fitness += f
+    total_fitness = 0
+    for gen in genom.genes:
+        total_fitness += gen.fitness
 
     genom.fitness = total_fitness
 
+
     return total_fitness
 
-
-
 m1 = load("data/p10i3000_wipe_m1.d")
-m2 = load("data/p10i3000_wipe_m2.d")
+#m2 = load("data/p10i3000_wipe_m2.d")
 
-ev = evaluate_p(m1,12,1000)
-ev = evaluate_p(m1,12,1000)
+
+ev = evaluate_p(m1)
+print(m1.fitness)
 print(ev)
 
+arr_room = []
+arr_teacher = []
+i = 0
 
-print(m2.fitness)
-exit()
-for i in m1.genes:
-    print(i.days,i.times,i.branch.teacher.name,i.branch.teacher.timeIntervals)
+teacher_cakisma_sayisi = 0
+room_cakisma_sayisi = 0
+
+for gen1 in m1.genes:
+    for gen2 in m1.genes:
+        for i in range(len(gen1.days)):
+            for j in range(len(gen2.days)):
+                if (gen1.branch.teacher.id == gen2.branch.teacher.id):
+                    if (gen1.days[i] == gen2.days[j]):
+                        g1time = gen1.times[i]
+                        g2time = gen2.times[j]
+                        start1 = int(g1time['begin'].split(":")[0])
+                        start2 = int(g2time['begin'].split(":")[0])
+                        end1 = int(g1time['end'].split(":")[0])
+                        end2 = int(g2time['end'].split(":")[0])
+                        g1room = gen1.rooms[i].id
+                        g2room = gen2.rooms[j].id
+                        if(g1room != g2room):
+                            if(max(start1,start2) < min(end1,end2)):
+                                str1 = gen1.branch.teacher.id , gen1.days[i] , g1time , g2time , g1room, g2room
+                                str2 = gen1.branch.teacher.id , gen2.days[j] , g2time , g1time , g2room, g1room
+                                if (str1 not in arr_teacher) and (str2 not in arr_teacher):
+                                    teacher_cakisma_sayisi = teacher_cakisma_sayisi + 1
+                                    arr_teacher.append(str1)
+
+                else:
+                    if(gen1.rooms[i].id == gen2.rooms[j].id):
+                        if (gen1.days[i] == gen2.days[j]):
+                            g1time = gen1.times[i]
+                            g2time = gen2.times[j]
+                            start1 = int(gen1.times[i]['begin'].split(":")[0])
+                            start2 = int(gen2.times[j]['begin'].split(":")[0])
+                            end1 = int(g1time['end'].split(":")[0])
+                            if (max(start1,start2) < min(end1,end2)):
+                                str1 = gen1.days[i] , g1time , g2time , g1room, g2room
+                                str2 = gen2.days[j] , g2time , g1time , g2room, g1room
+                                if (str1 not in arr_room) and (str2 not in arr_room):
+                                    room_cakisma_sayisi = room_cakisma_sayisi + 1
+                                    arr_room.append(str1)
+
+teacher_cakisma_sayisi = teacher_cakisma_sayisi / len(m1.genes)
+room_cakisma_sayisi = room_cakisma_sayisi / len(m1.genes)
+
+print("Ogretmen çakisma sayisi: ", teacher_cakisma_sayisi)
+print("Sinif çakisma sayisi: ", room_cakisma_sayisi)
